0850dac99a71de07986cdacfe62519406a2bb948
Artemol <67518760+Artemol@users.noreply.github.com>
2026-04-13 13:25:46 +0900

    add brew cask

--- hosts/bergamot.nix
+++ hosts/bergamot.nix
@@ -20,9 +20,28 @@
       upgrade = true;
     };
 
+    brews = [
+      "colima"
+      "docker-compose"
+      "typst"
+    ];
+
     casks = [
       "arc"
       "visual-studio-code"
+      "1password"
+      "raycast"
+      "alt-tab"
+      "chatgpt"
+      "clipy"
+      "dotnet-sdk"
+      "linear-linear"
+      "google-japanese-ime"
+      "font-hackgen-nerd"
+      "mactex"
+      "scroll-reverser"
+      "unity-hub"
+      "zotero"
     ];
   };
 
